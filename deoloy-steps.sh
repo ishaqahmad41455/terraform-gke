e5d66a7339bd61e9bb0c76d9e1154a72dfcc8fae
aquamo@gmail.com <103864675+ishaqahmad41455@users.noreply.github.com>
2024-12-11 13:19:48 +0500

    fixes

---
+++ deoloy-steps.sh
@@ -0,0 +1,29 @@
+########### DEPLOY APPLICATON IN GKE AUTOPOILOT MODE #######################
+
+Create a Docker Repository in Artifact Registory
+
+Build Docker Image using Dockerfile 
+
+Push the Docker image to Artifact registory   # use cicd do push the image 
+
+Create an Autopoilot Cluster in GKE using Terraform 
+
+Deploy the application in GKE Cluster using CI/CD Github Action # modify the cicd github action file (YML) To deploy the app
+
+Expose the App to the Internet
+
+
+
+########### DEPLOY APPLICATON IN GKE STANDRAD MODE #######################
+
+Create a Docker Repository in Artifact Registory
+
+Build Docker Image using Dockerfile 
+
+Push the Docker image to Artifact registory   # use cicd do push the image 
+
+Create an Autopoilot Cluster in GKE using Terraform 
+
+Deploy the application in GKE Cluster using CI/CD Github Action # modify the cicd github action file (YML) To deploy the app
+
+Expose the App to the Internet
